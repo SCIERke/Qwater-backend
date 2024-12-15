0432159375ffdfe4ee38b45943fea6cedd3405be
Peerapat Setsuk <peerapatsetsuk15@gmail.com>
2024-12-16 00:21:49 +0700

    Creating requirements files for deploy🛠

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ app.add_middleware(
     CORSMiddleware,
 allow_origins=["http://localhost:3000"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 
